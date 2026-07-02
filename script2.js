a33270a24b5decad0b0072c1fd42eb7005e7345a
Muhammad Siddique <billi.programmer@gmail.com>
2026-07-02 17:27:06 +0500

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,23 @@
+function createMember() {
+  alert("Create New Member clicked!");
+}
+
+function updateMember() {
+  alert("Update Existing Member clicked!");
+}
+
+function removeMember() {
+  alert("Remove Existing Member clicked!");
+}
+
+function readMembers() {
+  alert("Read Members clicked!");
+}
+
+function markAttendance() {
+  alert("Mark Attendances clicked!");
+}
+
+function fees() {
+  alert("Fees clicked!");
+}
